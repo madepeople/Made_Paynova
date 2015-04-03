c9b45c7889192242fa2604258461f0ffba50c9e4
Jonathan Selander <selander.jonathan@gmail.com>
2015-04-03 16:24:56 +0200

    first api call, get addresses. doesnt work, perhaps due to incorrect credentials

--- src/code/Paynova/Model/Payment/Abstract.php
+++ src/code/Paynova/Model/Payment/Abstract.php
@@ -20,9 +20,98 @@ class Made_Paynova_Model_Payment_Abstract
     protected $_canUseForMultishipping = true;
     protected $_canManageRecurringProfiles = false;
 
+    /**
+     * Retrieve global Paynova settings information from payment configuration
+     *
+     * @param string $field
+     * @param int|string|null|Mage_Core_Model_Store $storeId
+     *
+     * @return mixed
+     */
+    public function getGeneralConfigData($field, $storeId = null)
+    {
+        if (null === $storeId) {
+            $storeId = $this->getStore();
+        }
+        $path = 'payment/made_paynova_general/'.$field;
+        return Mage::getStoreConfig($path, $storeId);
+    }
+
+    /**
+     * Get the API URL depending on the test settings
+     *
+     * @return mixed
+     */
+    public function getApiUrl()
+    {
+        if ($this->getIsTest()) {
+            $url = $this->getGeneralConfigData('api_url_test');
+        } else {
+            $url = $this->getGeneralConfigData('api_url_live');
+        }
+        $url = preg_replace('#/+$#', '', $url);
+        return $url;
+    }
+
+    /**
+     * Determine if we are in test mode or not
+     *
+     * @return bool
+     */
+    public function getIsTest()
+    {
+        $testMode = (bool)$this->getGeneralConfigData('test');
+        return $testMode;
+    }
+
+    protected function _call($method, $parameters = null)
+    {
+        $method = '/' . preg_replace('#^/+#', '', $method);
+        $url = $this->getApiUrl() . $method;
+
+        $httpClient = new Zend_Http_Client($url);
+        $httpClient->setAdapter('Zend_Http_Client_Adapter_Curl');
+        $httpClient->setHeaders('Content-Type', 'application/json');
+
+        $username = $this->getGeneralConfigData('merchant_id');
+        $password = $this->getGeneralConfigData('password');
+        $httpClient->setAuth($username, $password, Zend_Http_Client::AUTH_BASIC);
+
+        $response = $httpClient->request();
+        if ($response->getStatus() !== 200) {
+            Mage::throwException('An error occurred when communicating with Paynova: ' . $response->getMessage() . ' (' . $response->getStatus() . ')');
+        }
+
+        $result = $response->getBody();
+        return $result;
+    }
+
+    /**
+     * Authorize a new payment
+     *
+     * @param Varien_Object $payment
+     * @param float $amount
+     */
     public function authorize(Varien_Object $payment, $amount)
     {
 
+    }
+
+    /**
+     * Get addresses for the supplied governemnt ID and country code.
+     *
+     * @param $countryCode
+     * @param $governmentId
+     */
+    public function getAddresses($countryCode, $governmentId)
+    {
+        $method = join('/', array(
+            'addresses',
+            $countryCode,
+            $governmentId
+        ));
+        $result = $this->_call($method);
+        return $result;
     }
 
 }
--- src/code/Paynova/controllers/UtilityController.php
+++ src/code/Paynova/controllers/UtilityController.php
@@ -1,9 +1,31 @@
 <?php
 
+/**
+ * Various utility actions
+ *
+ * @author jonathan@madepeople.se
+ */
 class Made_Paynova_UtilityController extends Mage_Core_Controller_Front_Action
 {
-    public function getAddressAction()
+
+    /**
+     * The get addresses call is proxied through magento because we want the
+     * possibility to save the returned information with the magento quote.
+     * Also, we might want throttling in the future.
+     *
+     * @return void
+     */
+    public function getAddressesAction()
     {
-        echo 'yo';
+        // We use the invoice model because getAddresses is part of
+        // paynova_abstract and the credentials are always the same
+        $governmentId = $this->getRequest()->getParam('government_id');
+        $countryCode = $this->getRequest()->getParam('country_code');
+
+        $method = Mage::getModel('made_paynova/payment_invoice');
+        $addresses = $method->getAddresses($countryCode, $governmentId);
+        $addressesJson = Mage::helper('core')->jsonEncode($addresses);
+        $this->getResponse()->setHeader('Content-Type', 'application/json');
+        $this->getResponse()->setBody($addressesJson);
     }
 }
